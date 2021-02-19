e1b56a88967a9f363bdc1abd610a9e5521e3a540
JamieDawson <jamierobertdawson@gmail.com>
2021-02-18 20:26:11 -0800

    getting ready to create routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,7 +8,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.unsubscribe(bodyParser.json());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+	console.log('MongoDB database connected yeah!!!');
+});
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
 
 app.listen(port, () => {
 	console.log(`server is one ${port}`);
